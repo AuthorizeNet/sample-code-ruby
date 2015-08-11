e6be6cd1a4697c69089dd0b2dcb9df21cd748719
brianmc <bmcmanus@authorize.net>
2015-08-10 22:13:53 -0700

    Added customer profile sample

---
+++ CustomerProfiles/CreateCustomerProfileFromTransaction.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+  require 'yaml'
+  require 'authorizenet'
+
+  include AuthorizeNet::API
+
+  config = YAML.load_file(File.dirname(__FILE__) + "/../credentials.yml")
+
+  transaction = Transaction.new(config['api_login_id'], config['api_transaction_key'], :gateway => :sandbox)
+
+  
+  request = CreateCustomerProfileFromTransactionRequest.new
+  request.transId = 2238251168
+  response = transaction.create_customer_profile_from_transaction(createProfReq)
+
+
+  if response.messages.resultCode == MessageTypeEnum::Ok
+    puts "Successfully created a customer profile from the transaction id #{request.transId}"
+  else
+    puts response.messages.messages[0].text
+    raise "Failed to create a customer profile from an existing transaction."
+  end
