50a0bc41554010a051604085aac56aebf2fcccdf
Laxmi Patha <laxmip5@vt.edu>
2015-08-13 15:02:49 -0700

    Updating the remaing Recurring Bill ruby files

---
+++ RecurringBilling/UpdateASubscription.rb
@@ -0,0 +1,42 @@
+require 'rubygems'
+  require 'yaml'
+  require 'authorizenet'
+
+  include AuthorizeNet::API
+
+  config = YAML.load_file(File.dirname(__FILE__) + "/../credentials.yml")
+ transaction = Transaction.new(config['api_login_id'], config['api_transaction_key'], :gateway => :sandbox)
+  #subscription = Subscription.new(config['api_login_id'], config['api_subscription_key'], :gateway => :sandbox)
+
+    request = ARBUpdateSubscriptionRequest.new
+    request.refId = '2238251168'
+    request.subscriptionId = '2790501'
+    request.subscription = ARBSubscriptionType.new
+    #request.subscriptionId = 100748
+    #request.subscription = ARBSubscriptionType.new
+    
+    request.subscription.payment = PaymentType.new
+    request.subscription.payment.creditCard = CreditCardType.new('4111111111111111','0120','123')
+
+    response = transaction.update_subscription(request)
+     
+    #validate the transaction was created
+    #expect(response.transactionResponse.transId).not_to eq("0")  
+    #{request.customerProfileId}
+
+
+if response != nil
+  if response.messages.resultCode == MessageTypeEnum::Ok
+    puts "Successfully updated a subscription "
+    puts response.messages.messages[0].code
+    puts response.messages.messages[0].text
+
+  else
+    #puts response.transactionResponse.errors.errors[0].errorCode
+    #puts response.transactionResponse.errors.errors[0].errorText
+    puts response.messages.messages[0].code
+    puts response.messages.messages[0].text
+    raise "Failed to get a subscriptions status"
+  end
+end
+
