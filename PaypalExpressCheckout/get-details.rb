6ee1e865711b8c86d6015f84debbce52a498d450
srathod <srathod@visa.com>
2015-10-08 16:18:32 +0530

    - Added samples for authorization-and-capture and get-details for Paypal Transaction.
    - Reviewed By: Ashish Kankaria(akankaria)

---
+++ PaypalExpressCheckout/get-details.rb
@@ -0,0 +1,39 @@
+require 'rubygems'
+  require 'yaml'
+  require 'authorizenet'
+
+  include AuthorizeNet::API
+
+  config = YAML.load_file(File.dirname(__FILE__) + "/../credentials.yml")
+
+  transaction = Transaction.new(config['api_login_id'], config['api_transaction_key'], :gateway => :sandbox)
+
+  request = CreateTransactionRequest.new
+  
+  payPalType = PayPalType.new()
+  payPalType.successUrl = "http://www.merchanteCommerceSite.com/Success/TC25262"
+  payPalType.cancelUrl = "http://www.merchanteCommerceSite.com/Success/TC25262"
+  
+  #standard api call to retrieve response
+  paymentType = PaymentType.new()
+  paymentType.payPal = payPalType
+  
+  request.transactionRequest = TransactionRequestType.new()
+  request.transactionRequest.payment = paymentType
+  request.transactionRequest.refTransId = "2241761234"
+  request.transactionRequest.transactionType = TransactionTypeEnum::GetDetailsTransaction
+  
+  response = transaction.create_transaction(request)
+  
+  #// If API Response is ok, go ahead and check the transaction response
+  if response.messages.resultCode == MessageTypeEnum::Ok
+    puts "Paypal Get Details successful."
+    puts "Response Code : #{response.transactionResponse.responseCode}"
+    puts "Shipping address : #{response.transactionResponse.shipTo.address}, #{response.transactionResponse.shipTo.city}, #{response.transactionResponse.shipTo.state}, #{response.transactionResponse.shipTo.country}"
+    puts "Payer ID : #{response.transactionResponse.secureAcceptance.PayerID}"
+  else
+    puts response.messages.messages[0].text
+    puts response.transactionResponse.errors.errors[0].errorCode
+    puts response.transactionResponse.errors.errors[0].errorText
+    raise "Paypal Get Details failed."
+  end
