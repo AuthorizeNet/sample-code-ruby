569cebda5891f8b53a0c2d0bdc1242fe9e9f8e4c
Srijan Misra <srmisra@WW975L29177B.visa.com>
2015-10-09 10:25:27 +0530

    Sample code for get settled batch list
    Adding sample for getting settled batch list for a merchant.
    
    Signed-off-by: Srijan Misra <srmisra@WW975L29177B.visa.com>

---
+++ contents/TransactionReporting/get-settled-batch-list.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+  require 'yaml'
+  require 'authorizenet'
+  require "date"
+
+  include AuthorizeNet::Reporting
+
+  config = YAML.load_file(File.dirname(__FILE__) + "/../credentials.yml")
+
+  transaction = Transaction.new(config['paypal_api_login_id'], config['paypal_api_transaction_key'], :gateway => :sandbox)
+
+  firstSettlementDate = Date.today - 31
+  lastSettlementDate = Date.today
+
+  puts "First settlement date: #{firstSettlementDate} Last settlement date: #{lastSettlementDate}"
+
+  response = transaction.get_settled_batch_list(from_date=DateTime.now()-(1 * 24), to_date=DateTime.now(), include_status=true)
+    
+  if response.result_code == "Ok"
+  	#puts "#{response.batch_list.to_yaml}"
+  	batchList = response.batch_list
+  	
+  	for child in batchList
+  		puts "Transaction Id: #{child.id}"
+      puts "Settlement Date: #{child.settled_at}"
+      puts "State: #{child.state}"
+      puts "Account Type: #{child.statistics[0].account_type}"
+      puts ""
+  	end
+  else
+    puts response.message_text
+    raise "Failed to fetch transaction list"
+  end
