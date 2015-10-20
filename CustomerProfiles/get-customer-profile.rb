dbf58f6e5e2a27a1bb95b9331db15cc798499247
namanbansal <namanbansal21@gmail.com>
2015-10-20 18:51:33 +0530

    + Display payment profile and shipping profile id in get-customer-profile

--- CustomerProfiles/get-customer-profile.rb
+++ CustomerProfiles/get-customer-profile.rb
@@ -10,13 +10,22 @@ require 'rubygems'
 
   
   request = GetCustomerProfileRequest.new
-  request.customerProfileId = '37115969'
+  request.customerProfileId = '36152115'
 
   response = transaction.get_customer_profile(request)
 
-
   if response.messages.resultCode == MessageTypeEnum::Ok
     puts "Successfully retrieved a customer with profile id is #{request.customerProfileId} and whose customer id is #{response.profile.merchantCustomerId}"
+    response.profile.paymentProfiles.each do |paymentProfile|
+      puts "Payment Profile ID #{paymentProfile.customerPaymentProfileId}"     
+    end
+    response.profile.shipToList.each do |ship|
+      puts "Shipping Details:"
+      puts "First Name #{ship.firstName}"
+      puts "Last Name #{ship.lastName}"
+      puts "Address #{ship.address}"
+      puts "Customer Address ID #{ship.customerAddressId}"
+    end
   else
     puts response.messages.messages[0].text
     raise "Failed to get customer profile information with id #{request.customerProfileId}"
