b059c6bbe072b38a6f0d1e49a2e7975021691aab
Srijan Misra <srmisra@WW975L29177B.visa.com>
2015-10-27 16:39:08 +0530

    Sample code for Capture Funds Authorized through another channel
    Adds sample code to capture funds authorized through another channel.
    
    Signed-off-by: Srijan Misra <srmisra@visa.com>

---
+++ PaymentTransactions/capture-funds-authorized-through-another-channel.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+  require 'yaml'
+  require 'authorizenet'
+
+  include AuthorizeNet::API
+
+  config = YAML.load_file(File.dirname(__FILE__) + "/../credentials.yml")
+
+  transaction = Transaction.new(config['api_login_id'], config['api_transaction_key'], :gateway => :sandbox)
+
+  request = CreateTransactionRequest.new
+
+  request.transactionRequest = TransactionRequestType.new()
+  request.transactionRequest.amount = 116.00
+  request.transactionRequest.payment = PaymentType.new
+  request.transactionRequest.payment.creditCard = CreditCardType.new('4111111111111111','0718') 
+  request.transactionRequest.transactionType = TransactionTypeEnum::CaptureOnlyTransaction
+  request.transactionRequest.authCode = "ROHNFQ"
+  
+  response = transaction.create_transaction(request)
+
+  if response.messages.resultCode == MessageTypeEnum::Ok
+    if response.transactionResponse != nil
+      puts "Success, Auth Code : #{response.transactionResponse.authCode}"
+    end
+  else
+    puts response.messages.messages[0].text
+    puts response.transactionResponse.errors.errors[0].errorCode
+    puts response.transactionResponse.errors.errors[0].errorText
+    raise "Failed to charge card."
+  end
