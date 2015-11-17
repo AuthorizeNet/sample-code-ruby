26e1cf9a40cd9298a5045b4a4f49db8583bcb256
nbansal <nbansal@visa.com>
2015-11-17 23:07:30 +0530

    + get-customer-payment-profile-list + get-subscription

---
+++ CustomerProfiles/get-customer-payment-profile-list.rb
@@ -0,0 +1,52 @@
+require 'rubygems'
+  require 'yaml'
+  require 'authorizenet'
+
+  include AuthorizeNet::API
+
+  config = YAML.load_file(File.dirname(__FILE__) + "/../credentials.yml")
+
+  transaction = Transaction.new(config['api_login_id'], config['api_transaction_key'], :gateway => :sandbox)
+  
+  #set the split tender ID here
+
+  #set the split tender status here.
+  #SplitTenderStatusEnum::Completed
+  #SplitTenderStatusEnum::Held
+  #SplitTenderStatusEnum::Voided
+  searchTypeEnum = CustomerPaymentProfileSearchTypeEnum::CardsExpiringInMonth
+  sorting = CustomerPaymentProfileSorting.new
+  orderByEnum = CustomerPaymentProfileOrderFieldEnum::Id
+  sorting.orderBy = orderByEnum
+  sorting.orderDescending = false
+  
+  paging = Paging.new
+  paging.limit = 1000
+  paging.offset = 1
+  
+  
+  request = GetCustomerPaymentProfileListRequest.new
+
+  request.searchType = searchTypeEnum
+  request.month = "2020-12"
+  request.sorting = sorting
+  request.paging = paging
+  
+  response = transaction.get_customer_payment_profile_list(request)
+  puts response.paymentProfiles.paymentProfile.length
+  puts response.paymentProfiles.paymentProfile[0].billTo.firstName
+  if response.messages.resultCode == MessageTypeEnum::Ok
+    puts "Successful got customer payment profile list"
+    puts response.messages.messages[0].code
+    puts response.messages.messages[0].text
+    puts "Total Result in Set = #{response.totalNumInResultSet}"
+    response.paymentProfiles.paymentProfile.each do |paymentProfile|
+      puts "Payment profile id = #{paymentProfile.customerPaymentProfileId}"
+      puts "First Name in Billing Address = #{paymentProfile.billTo.firstName}"
+      puts "Credit Card Number = #{paymentProfile.payment.creditCard.cardNumber}"
+    end
+  else
+    puts response.messages.messages[0].code
+    puts response.messages.messages[0].text
+    raise "Failed to get customer payment profile list"
+  end
