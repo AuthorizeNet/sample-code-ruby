d1855472bc7649b2e70bc9a367ea76330f640ba1
namanbansal <namanbansal21@gmail.com>
2015-12-07 16:07:22 +0530

    + updated get-unsettled-transaction-list.rb to use new model

--- TransactionReporting/get-unsettled-transaction-list.rb
+++ TransactionReporting/get-unsettled-transaction-list.rb
@@ -2,20 +2,24 @@ require 'rubygems'
   require 'yaml'
   require 'authorizenet'
 
-  include AuthorizeNet::Reporting
+  include AuthorizeNet::API
 
   config = YAML.load_file(File.dirname(__FILE__) + "/../credentials.yml")
   #merchant information
   transaction = Transaction.new(config['api_login_id'], config['api_transaction_key'], :gateway => :sandbox)
-  #get response from service
-  response = transaction.get_unsettled_transaction_list
-
-  if response.result_code == "Ok"
+  
+  request = GetUnsettledTransactionListRequest.new
+  
+  response = transaction.get_unsettled_transaction_list(request)
+  
+  if response.messages.resultCode == MessageTypeEnum::Ok
     unsettled_transactions = response.transactions
   
-    unsettled_transactions.each do |unsettled_transaction|
-      puts "Transaction #{unsettled_transaction.id} was submitted at #{unsettled_transaction.submitted_at}"
-    end    
+    response.transactions.transaction.each do |unsettled_transaction|
+      puts "Transaction #{unsettled_transaction.transId} was submitted at #{unsettled_transaction.submitTimeUTC}"
+      
+    end
+    puts "Total transaction received #{unsettled_transactions.transaction.length}"
   else
     puts "ERROR message: #{response.message_text}"
     puts "ERROR code: #{response.message_code}"
