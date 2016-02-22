e7797d852818ac7a6ab5cfac530fde4699cef8ee
namanbansal <namanbansal21@gmail.com>
2016-02-22 19:35:46 +0530

    + update test runner + fix bugs

--- PaymentTransactions/charge-customer-profile.rb
+++ PaymentTransactions/charge-customer-profile.rb
@@ -6,7 +6,7 @@ require 'rubygems'
 
   include AuthorizeNet::API
 
-  def charge_customer_profile()
+  def charge_customer_profile(customerProfileId = '36731856', customerPaymentProfileId = '33211899')
     config = YAML.load_file(File.dirname(__FILE__) + "/../credentials.yml")
   
     transaction = Transaction.new(config['api_login_id'], config['api_transaction_key'], :gateway => :sandbox)
@@ -16,9 +16,9 @@ require 'rubygems'
     request.transactionRequest = TransactionRequestType.new()
     request.transactionRequest.amount = ((SecureRandom.random_number + 1 ) * 150 ).round(2)
     request.transactionRequest.transactionType = TransactionTypeEnum::AuthCaptureTransaction
-    request.transactionRequest.profile = CustomerProfilePaymentType.new()
-    request.transactionRequest.profile.customerProfileId = "36731856"
-    request.transactionRequest.profile.paymentProfile = PaymentProfile.new("33211899")
+    request.transactionRequest.profile = CustomerProfilePaymentType.new
+    request.transactionRequest.profile.customerProfileId = customerProfileId
+    request.transactionRequest.profile.paymentProfile = PaymentProfile.new(customerPaymentProfileId)
   
     response = transaction.create_transaction(request)
   
