7b2a458c9445acafbde004537e64bd413458f84f
adavidw <aaron@infocreations.com>
2017-10-10 22:31:08 -0600

    shorten list of results from GetCustomerProfileIdsRequest

--- CustomerProfiles/get-customer-profile-ids.rb
+++ CustomerProfiles/get-customer-profile-ids.rb
@@ -17,10 +17,19 @@ require 'securerandom'
 
 
     if response.messages.resultCode == MessageTypeEnum::Ok
-      puts "Successfully retrieved customer ids:"
-      response.ids.numericString.each do |id|
-        puts id
+      puts "Successfully retrieved customer IDs. Number of IDs returned: #{response.ids.numericString.count}"
+      # There's no paging options in this API request; the full list is returned every call.
+      # If the result set is going to be large, for this sample we'll break it down into smaller
+      # chunks so that we don't put 72,000 lines into a log file
+      puts "First 20 results:"
+      for profileId in 0..19 do
+        puts response.ids.numericString[profileId]
       end
+      # If we wanted to just return the whole list, we'd do something like this:
+      #  
+      # response.ids.numericString.each do |id|
+      #   puts id
+      # end
 
     else
       puts response.messages.messages[0].text
