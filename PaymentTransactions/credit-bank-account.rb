35355772d6cbd9472ea6a885ba364c5608057e02
Lakshmi Gangumalla <lakshmi9414@gmail.com>
2018-08-28 12:31:03 +0530

    testing only credit bank account and chnaged routing number to 122000661 and account number to random

--- PaymentTransactions/credit-bank-account.rb
+++ PaymentTransactions/credit-bank-account.rb
@@ -15,7 +15,9 @@ require 'securerandom'
     request.transactionRequest = TransactionRequestType.new()
     request.transactionRequest.amount = ((SecureRandom.random_number + 1 ) * 15 ).round(2)
     request.transactionRequest.payment = PaymentType.new
-    request.transactionRequest.payment.bankAccount = BankAccountType.new('checking','121042882','123456789', 'John Doe','PPD','Wells Fargo Bank NA','101') 
+	//Generate random bank account number
+	randomAccountNumber= rand(100000000,9999999999);
+    request.transactionRequest.payment.bankAccount = BankAccountType.new('checking','122000661',randomAccountNumber, 'John Doe','PPD','Wells Fargo Bank NA','101') 
     request.transactionRequest.transactionType = TransactionTypeEnum::RefundTransaction
     
     response = transaction.create_transaction(request)
