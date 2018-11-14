e2492a7d90ec762abf1f1e9521b9bfef838f1b03
naina <nacharje@visa.com>
2018-11-14 10:36:21 +0530

    PR1992-getaujobdetails-comment

--- TransactionReporting/get-aujob-details.rb
+++ TransactionReporting/get-aujob-details.rb
@@ -11,19 +11,19 @@ require 'certified'
   def get_aujob_Details()
    config = YAML.load_file(File.dirname(__FILE__) + "/../credentials.yml")
    transaction = AuthorizeNet::API::Transaction.new(config['api_login_id'], config['api_transaction_key'], :gateway => :sandbox)
-     #@month = '2006-11'
+     
      modifiedTypeFilter = "all"
-     refId='123456'
+     refId = '123456'
      paging = Paging.new
      paging.limit = 100
      paging.offset = 1
      # Build the request object
-     request= GetAUJobDetailsRequest.new
+     request = GetAUJobDetailsRequest.new
      #request = AuthorizeNet::API::GetAUJobDetailsRequest.new
      request.paging = paging
-     request.month="2018-08"
-     request.modifiedTypeFilter=modifiedTypeFilter
-     request.refId=refId
+     request.month = "2018-08"
+     request.modifiedTypeFilter = modifiedTypeFilter
+     request.refId = refId
      response = transaction.get_aujob_details(request)
      #puts response.
      puts response.messages.resultCode
@@ -34,49 +34,49 @@ require 'certified'
            return response;
          else
            puts response.auDetails
-           puts "Refrence id is :#{response.refId}"
-           puts" message is : "
+           puts "Refrence id is : #{response.refId}"
+           puts "message is : "
            puts response.messages.messages[0].code        
            puts response.messages.messages[0].text
-           puts "  Total number in result set: #{response.totalNumInResultSet}"
-           update= response.auDetails[0].auUpdate
+           puts "Total number in result set : #{response.totalNumInResultSet}"
+           update = response.auDetails[0].auUpdate
            update.each do|update|
              puts "update start"
              puts "Customer profile id : #{update.customerProfileID}"
-             puts "Customer Payment Profile ID:#{update.customerPaymentProfileID}"
-             puts "Update Time (UTC): #{update.updateTimeUTC}"
-             puts "Reason Code:#{update.auReasonCode}"
-             puts "Reason Description:#{update.reasonDescription}"
-             subscription=update.subscriptionIdList[0].subscriptionId
+             puts "Customer Payment Profile ID : #{update.customerPaymentProfileID}"
+             puts "Update Time (UTC) : #{update.updateTimeUTC}"
+             puts "Reason Code : #{update.auReasonCode}"
+             puts "Reason Description : #{update.reasonDescription}"
+             subscription = update.subscriptionIdList[0].subscriptionId
              if subscription != nil
-                 puts"SubscriptionId list:"
-                 puts"SubscriptionId :#{subscription[0]}"
+                 puts "SubscriptionId list :"
+                 puts "SubscriptionId : #{subscription[0]}"
              end   
-             puts"New card details"
-              puts "Card Number:#{update.newCreditCard.cardNumber}"
-              puts "New Expiration Date:#{update.newCreditCard.expirationDate}"
-              puts "New Card Type: #{update.newCreditCard.cardType}"
-              puts"Old card details"
-              puts "Card Number:#{update.oldCreditCard.cardNumber}"
-              puts "Expiration Date:#{update.oldCreditCard.expirationDate}"
-              puts "Old Card Type: #{update.oldCreditCard.cardType}"
+             puts "New card details"
+              puts "Card Number : #{update.newCreditCard.cardNumber}"
+              puts "New Expiration Date : #{update.newCreditCard.expirationDate}"
+              puts "New Card Type : #{update.newCreditCard.cardType}"
+              puts "Old card details"
+              puts "Card Number : #{update.oldCreditCard.cardNumber}"
+              puts "Expiration Date : #{update.oldCreditCard.expirationDate}"
+              puts "Old Card Type : #{update.oldCreditCard.cardType}"
             end
-            delete=  response.auDetails[0].auDelete
+            delete =  response.auDetails[0].auDelete
             delete.each do|delete|
               puts "Customer profile id : #{delete.customerProfileID}"
-              puts "Customer Payment Profile ID:#{delete.customerPaymentProfileID}"
-             puts "Update Time (UTC): #{delete.updateTimeUTC}"
-              puts "Reason Code:#{delete.auReasonCode}"
-              puts "Reason Description:#{delete.reasonDescription}"
-              subscription=delete.subscriptionIdList[0].subscriptionId
+              puts "Customer Payment Profile ID : #{delete.customerPaymentProfileID}"
+              puts "Update Time (UTC) : #{delete.updateTimeUTC}"
+              puts "Reason Code : #{delete.auReasonCode}"
+              puts "Reason Description : #{delete.reasonDescription}"
+              subscription = delete.subscriptionIdList[0].subscriptionId
               if subscription != nil
-                puts"SubscriptionId list:"
-                puts"SubscriptionId :#{subscription[0]}"
+                puts "SubscriptionId list :"
+                puts "SubscriptionId : #{subscription[0]}"
               end  
-              puts"credit card details"
-              puts "Card Number:#{delete.creditCard.cardNumber}"
-              puts " Expiration Date:#{delete.creditCard.expirationDate}"
-              puts " Card Type: #{delete.creditCard.cardType}"
+              puts "credit card details"
+              puts "Card Number : #{delete.creditCard.cardNumber}"
+              puts "Expiration Date : #{delete.creditCard.expirationDate}"
+              puts "Card Type : #{delete.creditCard.cardType}"
             end
            
           end
